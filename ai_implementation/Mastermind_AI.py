2d6fdab2049b4ca1def487856d9d6010f0d12058
s153242 <48475510+s153242@users.noreply.github.com>
2019-03-28 16:59:37 +0100

    Update Mastermind_AI.py

--- ai_implementation/Mastermind_AI.py
+++ ai_implementation/Mastermind_AI.py
@@ -1,35 +1,31 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar 19 08:59:15 2019
-
-@author: Anders Thuesen
-"""
 from collections import Counter
 from itertools import product
-import random
 
 
-def evaluate(guess,secret_code):
-    matches = sum((Counter(secret_code) & Counter(guess)).values())   
+def evaluate(guess,code):
+    #This is the evaluation function that evaluate the guess to another code
+    #It returns the amount of bulls and cows. 
+    #Finding all the matches
+    All_matches = sum((Counter(code) & Counter(guess)).values())   
+    #Finding the bulls, the correct color and correct placement
+    bulls = sum(i == j for i, j in zip(guess, code))
+    #Cows is thus    
+    cows = All_matches - bulls
     
-    correct_color = sum(i == j for i, j in zip(guess, secret_code))
-    #zip command merge two lists into one.
-    
-    guessed_color = matches - correct_color
-    
-    return correct_color, guessed_color
+    return bulls, cows
   
 
 
 def MAX_VALUE(guess,States):
-           
+    #The purpose of this function is to evaluate the guess to all the possible states
+    #and then return the score (bulls and cows) that is the most occurent.      
+    #Evaluation_list is being filled with all evaluations with respect to the guess.
     evaluation_list = []
     for i in range(len(States)):
-        evaluation_list.append(evaluate(guess,States[i]))
-        
-    #This step counts the amount every score option occures for evaluation of the guess with all possible codes.
+        evaluation_list.append(evaluate(guess,States[i]))  
+    #This step counts the amount every score option occures in evaluation_list.
     evaluation_list_count = Counter(evaluation_list).values()
-
+    #Taking the max score. 
     MAX_count = max(evaluation_list_count)
     
     return MAX_count
@@ -37,36 +33,38 @@ def MAX_VALUE(guess,States):
     
 #MAX_VALUE(All_Codes)
 
-def MINIMAX(secret_code,All_Codes):
-    #This function go through all the possible states with same evaluation as the previous guess and then select the ones with minimum amount of states.
-    best_guess = "AABB"
-    minimum = 1296
+def MINIMAX(code,All_Codes):
+    #This function go through all the possible states with same evaluation as the previous guess
+    #and then select the best guess based on the one with the smallest amount of states.
+    #Best initial guess is found from earler investigations with the AI 
+    best_guess = "YYRR"
+    minimum = len(All_Codes)
     States = All_Codes
+    guess_count = 1
     while True:
-        feedback = evaluate(best_guess,secret_code)
-        
-        print("Guess {}: feedback {}".format(best_guess, feedback))
-        if best_guess == secret_code:
+        #Evaluate the guess to the secret code
+        eval_guess = evaluate(best_guess,code)
+        print("AI's guess nr. {}: Guess {}. Bulls and Cows: {}".format(guess_count, best_guess,eval_guess))
+        #Terminal test to break the loop if the guess is correct.
+        if best_guess == code:
+            print("The AI have guessed the secret code {} in {} tries".format(code,guess_count))
             break
-        States = [c for c in States if evaluate(best_guess, c) == feedback]
-        print("States {}".format(States))
-        print("Amount of states {}".format(len(States)))
+        
+        #This step removes the states that do not have the same evaluation as eval_guess
+        States = [i for i in States if evaluate(best_guess, i) == eval_guess]
+        print("{} states have similar evaluation: {}".format(len(States),States))
+        
         if len(States) == 1:
             best_guess = States[0]
+            
+        #Loops through the possible states and take the one with the min-max value. 
         else:
             for i in range(len(States)):
                 MAX_count = MAX_VALUE(States[i],States)
                 if MAX_count < minimum:
                     minimum = MAX_count
                     best_guess = States[i]
-                if States[i] == secret_code:
+                if States[i] == code:
                     break
-         
-            
-        print(best_guess)
-
-All_Codes = [''.join(c) for c in product('ABCDEF', repeat=4)]
-
-MINIMAX("EFCA",All_Codes)
-
-
+                                    
+        guess_count += 1  
